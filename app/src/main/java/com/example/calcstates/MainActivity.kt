21375926a618edc7c1af5531d008e4d1f2a50071
Cristina Jung <cristijung@gmail.com>
2023-08-16 14:30:21 -0300

    start pronto

--- app/src/main/java/com/example/calcstates/MainActivity.kt
+++ app/src/main/java/com/example/calcstates/MainActivity.kt
@@ -16,6 +16,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -54,8 +55,9 @@ fun TipTimeLayout() {
                 .align(alignment = Alignment.Start)
         )
         Text(
-            text = stringResource(R.string.tip_amount, "$0.00"),
-            style = MaterialTheme.typography.displaySmall
+            text = stringResource(R.string.tip_amount, "R$ 0.00"),
+            style = MaterialTheme.typography.displaySmall,
+            color = Color.DarkGray
         )
         Spacer(modifier = Modifier.height(150.dp))
     }
