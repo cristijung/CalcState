b439cae4693eea87783dd58e841f37c76f781300
Cristina Jung <cristijung@gmail.com>
2023-08-17 16:26:56 -0300

    ajuste de text field

--- app/src/main/java/com/example/calcstates/MainActivity.kt
+++ app/src/main/java/com/example/calcstates/MainActivity.kt
@@ -1,7 +1,7 @@
 
 package com.example.calcstates
 
-import android.annotation.SuppressLint
+
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -18,7 +18,10 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -73,12 +76,12 @@ fun TipTimeLayout() {
     }
 }
 
-@SuppressLint("UnrememberedMutableState")//verificar isso depois
+
 @Composable
 fun EditNumberField(modifier: Modifier = Modifier) {
-    var amountInput = mutableStateOf("0")
+    var amountInput by remember { mutableStateOf("") }
     TextField(
-        value = amountInput.value,
+        value = "",
         onValueChange = {},
         modifier = modifier
     )
